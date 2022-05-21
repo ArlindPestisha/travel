d4af262a84bc05cf24061ab1714fd16f644741eb
ArlindPestisha <arlind.pestisha@hyperisland.se>
2022-05-21 19:36:43 +0200

    adding Google Map and config the api in google cloud adding some style

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { GoogleMapReact } from 'google-map-react'
-import {Paper, Typography, useMediaQuery} from '@material-ui/core'
-import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined'
-import { Rating } from '@material-ui/lab'
+
+import GoogleMapReact from "google-map-react";
+
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import { Rating } from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const coordinates = { lat: 0, lng: 0 };
   return (
-    <div>Map</div>
-  )
-}
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyByl8ZMqrDaUhVaSxn_ySTbEWi4z-m5gHs" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
 
-export default Map
+export default Map;
